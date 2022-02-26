45fad354ad549b28a0ea2d90d23348f719918590
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-26 22:21:31 +0100

    modified files

---
+++ src/Hooks/useMovieFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+import apiSettings from "../API";
+
+const useMovieFetch = (movieId) => {
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        setLoading(true)
+        setError(false)
+
+        const movie = await apiSettings.fetchMovie(movieId);
+        const credits = await apiSettings.fetchCredits(movieId);
+
+        //Get directors
+        const directors = credits.crew.filter(
+          member => member.job === 'Director'
+        );
+
+        setState({
+          ...movie,
+          actors: credits.cast,
+          directors
+        })
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+      }
+    }
+    fetchMovie()
+  }, [movieId])
+
+  return { state, loading, error }
+}
+
+export default useMovieFetch;
