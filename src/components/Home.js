b74d6a68d16990fa0db2faa7f4cb3a14c477e80b
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-26 05:29:03 +0100

    set app Spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 //Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch';
@@ -44,6 +45,7 @@ const Home = () => {
           />
         ))}
       </Grid>
+      <Spinner />
 
 
     </div>
